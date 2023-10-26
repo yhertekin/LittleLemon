a48d838d6b0009231059f699c55d194570ecc3da
yhertekin <yhertekin@gmail.com>
2023-10-26 23:31:18 +0300

    update

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -1,7 +1,80 @@
+import Image from "next/image";
 import React from "react";
+import "./CartPage.css";
 
 const CartPage = () => {
-	return <div>CartPage</div>;
+    return (
+        <div className='cart-page'>
+            <div className='cart-page__products'>
+                <div className='cart-page__product'>
+                    <Image src='/temporary/p1.png' alt='product image' width={100} height={100} />
+                    <div>
+                        <h1 className='cart-page__product__title'>Scillian</h1>
+                        <span className='cart-page__product__size'>Large</span>
+                    </div>
+                    <h2 className='cart-page__product__price'>$79.90</h2>
+                    <Image
+                        className='cart-page__product__remove'
+                        src='/icons/Delete.svg'
+                        alt='remove product'
+                        width={24}
+                        height={24}
+                    />
+                </div>
+                <div className='cart-page__product'>
+                    <Image src='/temporary/p1.png' alt='product image' width={100} height={100} />
+                    <div>
+                        <h1 className='cart-page__product__title'>Scillian</h1>
+                        <span className='cart-page__product__size'>Large</span>
+                    </div>
+                    <h2 className='cart-page__product__price'>$79.90</h2>
+                    <Image
+                        className='cart-page__product__remove'
+                        src='/icons/Delete.svg'
+                        alt='remove product'
+                        width={24}
+                        height={24}
+                    />
+                </div>
+                <div className='cart-page__product'>
+                    <Image src='/temporary/p1.png' alt='product image' width={100} height={100} />
+                    <div>
+                        <h1 className='cart-page__product__title'>Scillian</h1>
+                        <span className='cart-page__product__size'>Large</span>
+                    </div>
+                    <h2 className='cart-page__product__price'>$79.90</h2>
+                    <Image
+                        className='cart-page__product__remove'
+                        src='/icons/Delete.svg'
+                        alt='remove product'
+                        width={24}
+                        height={24}
+                    />
+                </div>
+            </div>
+
+            <div className='cart-page__payment'>
+                <div className='flex justify-between'>
+                    <span className=''>Subtotal (3 items)</span>
+                    <span className=''>$81.70</span>
+                </div>
+                <div className='flex justify-between'>
+                    <span className=''>Service Cost</span>
+                    <span className=''>$0.00</span>
+                </div>
+                <div className='flex justify-between'>
+                    <span className=''>Delivery Cost</span>
+                    <span className='text-green-500'>FREE!</span>
+                </div>
+                <hr className='my-2' />
+                <div className='flex justify-between'>
+                    <span className=''>TOTAL</span>
+                    <span className='font-bold'>$81.70</span>
+                </div>
+                <button className='bg-red-500 text-white p-3 rounded-md w-1/2 self-end'>CHECKOUT</button>
+            </div>
+        </div>
+    );
 };
 
 export default CartPage;
--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,7 +1,50 @@
 import React from "react";
+import "./LoginPage.css";
+import Image from "next/image";
+import Link from "next/link";
 
 const LoginPage = () => {
-	return <div>LoginPage</div>;
+    return (
+        <div className='login-page'>
+            <div className='login-page__container'>
+                <div className='login-page__container__image'>
+                    <Image className='object-cover' src='/images/loginBg.png' alt='' fill />
+                </div>
+                <div className='login-page__container__form'>
+                    <h1 className='login-page__container__form__title'>Welcome</h1>
+                    <p>Login to your account or create a new one using social buttons</p>
+
+                    <button className='login-page__container__form__social'>
+                        <Image
+                            className='object-contain'
+                            src='/images/google.png'
+                            alt='google'
+                            width={20}
+                            height={20}
+                        />
+                        <span>Sign in with Google</span>
+                    </button>
+                    <button className='login-page__container__form__social'>
+                        <Image
+                            className='object-contain'
+                            src='/images/facebook.png'
+                            alt='facebook'
+                            width={20}
+                            height={20}
+                        />
+                        <span>Sign in with Facebook</span>
+                    </button>
+                    <p className='text-sm'>
+                        Have a problem?
+                        <Link className='underline' href='/'>
+                            {" "}
+                            Contact us
+                        </Link>
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default LoginPage;
